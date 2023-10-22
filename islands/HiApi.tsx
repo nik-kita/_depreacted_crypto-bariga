cb521bf338021f15b7677cb899e9e5c135d5f839
nik <nikita.nodejs@gmail.com>
2023-10-22 13:50:36 +0200

    draft ws connection to server

--- islands/HiApi.tsx
+++ islands/HiApi.tsx
@@ -2,13 +2,52 @@ import { useSignal } from "@preact/signals";
 import CssHead from "../components/CssHead.tsx";
 
 export default function HiApi() {
+  let toggleWs = false;
   const apiRes = useSignal(null as object | string | null);
+  const click = () => {
+    toggleWs = !toggleWs;
+
+    let ws: WebSocket | null = null;
+
+    if (toggleWs) {
+      const headers = new Headers();
+
+      headers.append("auth", "me");
+
+      ws = new WebSocket("ws://localhost:3000/monitor");
+
+      ws.onopen = () => {
+        apiRes.value = "Websocket was successfully opened!";
+      };
+
+      ws.onmessage = (ev) => {
+        apiRes.value = ev.data;
+      };
+
+      ws.onclose = () => {
+        apiRes.value = "Websocket was closed.";
+      };
+    } else {
+      apiRes.value = null;
+
+      if (!ws) {
+        console.warn("hmm... where is ws? i want to close it!");
+
+        return;
+      }
+
+      (ws as WebSocket).close();
+    }
+  };
 
   return (
     <>
       <CssHead importMetaUrl={import.meta.url} />
-      {apiRes.value &&
-        <pre class="pre">{JSON.stringify(apiRes.value, null, 2)}</pre>}
+      <div class="wrapFlex">
+        <button onClick={click}>Hello API!</button>
+        {apiRes.value &&
+          <pre class="pre">{JSON.stringify(apiRes.value, null, 2)}</pre>}
+      </div>
     </>
   );
 }
