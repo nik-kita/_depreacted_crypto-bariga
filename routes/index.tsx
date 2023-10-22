0fdbeee84ad703c8784bca639350505556823f40
nik <nikita.nodejs@gmail.com>
2023-10-22 21:12:41 +0200

    simple draft redirect if not login

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,13 +1,36 @@
+import { defineRoute } from "$fresh/src/server/defines.ts";
+import { getCookies } from "$std/http/cookie.ts";
 import CssHead from "../components/CssHead.tsx";
-import HiApi from "../islands/HiApi.tsx";
 
-export default function Home() {
+type SessionCookies = {
+  session: string;
+};
+
+type RedirectCookies = {
+  location: string;
+};
+
+export default defineRoute((req, ctx) => {
+  /**
+   * is login ? welcome : redirect
+   */
+  const cookies = getCookies(req.headers) as Partial<SessionCookies>;
+
+  if (!cookies.session) {
+    return new Response(null, {
+      status: 307,
+      headers: new Headers(
+        {
+          location: "/sign-in",
+        } satisfies RedirectCookies,
+      ),
+    });
+  }
+
   return (
     <>
       <CssHead importMetaUrl={import.meta.url} />
-      <div class="wrapper">
-        <HiApi />
-      </div>
+      <h1>Welcome!</h1>
     </>
   );
-}
+});
