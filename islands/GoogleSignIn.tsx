075efea3ed9e76f77d68cfe2aad582065e67c3c3
nik <nikita.nodejs@gmail.com>
2023-10-23 19:58:50 +0200

    useEffect:useRef => try to load gButton correctly

--- islands/GoogleSignIn.tsx
+++ islands/GoogleSignIn.tsx
@@ -1,43 +1,48 @@
 import { Head } from "$fresh/runtime.ts";
+import { useEffect, useRef } from "preact/hooks";
+import CssHead from "../components/CssHead.tsx";
 
 type Props = {
   OAUTH_2_CLIENT_ID_WEB_1: string;
 };
 
-function handleGoogleSignInRes(response: unknown) {
-  console.log(response);
-}
-
 export default function GoogleSignIn(props: Props) {
+  const gButton = useRef(null);
+
+  useEffect(() => {
+  }, [gButton.current]);
+
   return (
     <>
+      <CssHead importMetaUrl={import.meta.url} />
       <Head>
-        <script src="https://accounts.google.com/gsi/client" async defer></script>
+        <script src="https://accounts.google.com/gsi/client" async></script>
       </Head>
       <div>
-        <h1>defer</h1>
-        <div
-          id="g_id_onload"
-          data-client_id={props.OAUTH_2_CLIENT_ID_WEB_1}
-          data-context="signin"
-          data-ux_mode="popup"
-          data-callback={handleGoogleSignInRes.name}
-          data-auto_prompt="false"
-        >
-        </div>
-        <script>
-          {handleGoogleSignInRes.toString()}
-        </script>
+        <div ref={gButton}>
+          <div
+            id="g_id_onload"
+            data-client_id={props.OAUTH_2_CLIENT_ID_WEB_1}
+            data-context="use"
+            data-ux_mode="popup"
+            data-callback="handleGoogleOneTapAuth"
+            data-nonce=""
+            data-auto_select="true"
+            data-itp_support="true"
+          >
+          </div>
 
-        <div
-          class="g_id_signin"
-          data-type="standard"
-          data-shape="rectangular"
-          data-theme="outline"
-          data-text="signin_with"
-          data-size="large"
-          data-logo_alignment="left"
-        >
+          <div
+            class="g_id_signin"
+            data-type="standard"
+            data-shape="pill"
+            data-theme="filled_black"
+            data-text="continue_with"
+            data-size="large"
+            data-locale="en-US"
+            data-logo_alignment="left"
+          >
+          </div>
         </div>
       </div>
     </>
