52d1dcebe4ccdbf4d67f5c98bde7d8aa7441c69b
nik <nikita.nodejs@gmail.com>
2023-10-23 03:38:32 +0200

    1

--- islands/GoogleSignIn.tsx
+++ islands/GoogleSignIn.tsx
@@ -9,22 +9,22 @@ function handleGoogleSignInRes(response: unknown) {
 export default function GoogleSignIn(props: Props) {
   return (
     <>
-      <h1>G00</h1>
-      <script src="https://accounts.google.com/gsi/client" async></script>
-      <div
-        id="g_id_onload"
-        data-client_id={props.OAUTH_2_CLIENT_ID_WEB_1}
-        data-context="signin"
-        data-ux_mode="popup"
-        data-callback={handleGoogleSignInRes.name}
-        data-auto_prompt="false"
-      >
-      </div>
-      <script>
-        {handleGoogleSignInRes.toString()}
-      </script>
-
       <div class="parentFlexRowCenter blockSizeFullV">
+        <h1>1</h1>
+        <script src="https://accounts.google.com/gsi/client" async></script>
+        <div
+          id="g_id_onload"
+          data-client_id={props.OAUTH_2_CLIENT_ID_WEB_1}
+          data-context="signin"
+          data-ux_mode="popup"
+          data-callback={handleGoogleSignInRes.name}
+          data-auto_prompt="false"
+        >
+        </div>
+        <script>
+          {handleGoogleSignInRes.toString()}
+        </script>
+
         <div
           class="g_id_signin"
           data-type="standard"
